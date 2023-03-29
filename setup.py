7b545e8d2a0845c7207bb0973c4d6a70a0f843da
Bird <mostafa_shaeri_tj@yahoo.com>
2023-03-29 02:29:44 -0700

    Remove History

--- setup.py
+++ setup.py
@@ -3,15 +3,12 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     README = readme_file.read()
 
-with open('HISTORY.md') as history_file:
-    HISTORY = history_file.read()
-
 setup_args = dict(
     name='flask_sqlalchemy_report',
     version='0.1.8',
     description='A useful simple to use tool to turn your sql query into a beautiful report html table',
     long_description_content_type="text/markdown",
-    long_description=README + '\n\n' + HISTORY,
+    long_description=README ,
     license='GNU',
     packages=find_packages(),
     author='M.Shaeri',
